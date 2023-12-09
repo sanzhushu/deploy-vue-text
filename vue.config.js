52e1606bf4d529d07b45e5a7b24eeb2afa53e3dc
BYEBYE <475748430@qq.com>
2023-12-10 01:42:45 +0800

    two

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    proxy: 'http://111.229.247.11:3000/'
+    // proxy: 'http://111.229.247.11:3000/'
   }
 
 })
